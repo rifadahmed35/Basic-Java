162202f9eeafbc105304a5b012cb90413faabe82
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-24 22:49:47 +0100

    First PR

---
+++ src/Floyds_triangle.java
@@ -0,0 +1,12 @@
+public class Floyds_triangle {
+    public static void main(String[] args) {
+        int N, row, col, count=0;
+        N=4;
+        for(row=1; row<=N; row++){
+            for(col=1; col<=row; col++){
+                System.out.print(" " + ++count);
+            }
+            System.out.println();
+        }
+    }
+}
