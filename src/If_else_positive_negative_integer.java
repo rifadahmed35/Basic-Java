d3d057f5be0a4e55b5b5e57a094e0b4aa85f260f
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-26 00:51:24 +0100

    All of the basic of Java.

---
+++ src/If_else_positive_negative_integer.java
@@ -0,0 +1,11 @@
+public class If_else_positive_negative_integer {
+    public static void main(String[] args) {
+        int i=10;
+        if(i>=0){
+            System.out.println("The number is posotive.");
+        } else {
+            System.out.println("The number is negative");
+        }
+
+    }
+}
---
+++ src/Odd_even_no_if_else.java
@@ -0,0 +1,12 @@
+public class Odd_even_no_if_else {
+    public static void main(String[] args)
+    {
+        int i;
+        i=11;
+        if(i%2==0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
+}
