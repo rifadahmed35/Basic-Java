0186acf32d49219697752302d43825c61f825947
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-27 00:02:09 +0200

    All the basic of Java.

---
+++ src/Loop_do_while_odd_even_number.java
@@ -0,0 +1,9 @@
+public class Loop_do_while_odd_even_number {
+    public static void main(String[] args) {
+        int i=2;
+        do{
+            System.out.println(i);
+            i= i+2;
+        }while(i<=10);
+    }
+}
