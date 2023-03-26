497b94bfd4062c1284d0e36be2fd8a61266fca66
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-26 01:28:49 +0100

    All the basic of Java.

---
+++ src/Loop_for_factorial_no.java
@@ -0,0 +1,10 @@
+public class Loop_for_factorial_no {
+    public static void main(String[] args) {
+        int i, num, fact=1;
+        num=4;
+        for (i=1; i<=num; i++){
+            fact = fact*i;
+        }
+        System.out.println(fact);
+    }
+}
---
+++ src/Loop_for_generate_table.java
@@ -0,0 +1,8 @@
+public class Loop_for_generate_table {
+    public static void main(String[] args) {
+        int i, N=10;
+        for (i=1; i<=10; i++){
+            System.out.println(N + "*" + i + "=" + N*i);
+        }
+    }
+}
