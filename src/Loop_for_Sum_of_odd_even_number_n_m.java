d2cf4ea247aa209461ca2800b46ccce2e86cf88f
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-28 23:13:30 +0200

    All the basic of Java.

---
+++ src/Loop_for_Sum_of_odd_even_number_n_m.java
@@ -0,0 +1,13 @@
+public class Loop_for_Sum_of_odd_even_number_n_m {
+    public static void main(String[] args) {
+        int i, sum=0;
+        for(i=1; i<=10; i++){
+            if (i%2==0){
+                continue;
+            }
+            sum = sum + i; }
+        {
+            System.out.println(sum);
+        }
+    }
+}
