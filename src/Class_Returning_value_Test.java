7b5bc1074f85725d730d5591ef6967f9cf22d99e
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-04-15 02:58:25 +0200

    All the basic of Java.

---
+++ src/Class_Returning_value_Test.java
@@ -0,0 +1,16 @@
+class ReturningValueDemo {
+    int square(int value) {
+        return value * value;
+    }
+}
+public class Class_Returning_value_Test {
+    public static void main(String[] args) {
+        ReturningValueDemo obj1 = new ReturningValueDemo();
+        int result = obj1.square(4);
+        System.out.println("The square of the value is: "+result);
+
+        ReturningValueDemo obj2 = new ReturningValueDemo();
+        int result2 = obj2.square(3);
+        System.out.println("The square of the value is: "+result2);
+    }
+}
