c98aebea3f5df1877b7df998b041180e058a4922
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-04-25 01:10:49 +0200

    Encapsulation : Way of setter and getter method.

---
+++ src/Encapsulation_setter_getter_method.java
@@ -0,0 +1,30 @@
+class Person1 {
+    private String name;
+    private int age;
+
+    // Manually:
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Encapsulation_setter_getter_method {
+    public static void main(String[] args) {
+        Person1 p1 = new Person1();
+        p1.setName("Rifad");
+        System.out.println("Name: " +p1.getName());
+        p1.setAge(27);
+        System.out.println("Name: " +p1.getAge());
+    }
+}
